4abc9bf2f5ada36e62a6f848bb627a900630adcf
Mehul-Bisht <mehulbishtanna@gmail.com>
2020-12-24 20:26:11 +0530

    change

--- app/src/main/java/com/example/gittester/MainActivity.kt
+++ app/src/main/java/com/example/gittester/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //this is the first addition to branch
+
+        //this is the second addition to branch
     }
 }
